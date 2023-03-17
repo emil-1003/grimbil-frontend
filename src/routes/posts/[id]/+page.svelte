839f59242dd8c5d9f12d2fe3494aa225e6cd4eff
Emil Andersen <emiand@norlys.dk>
2023-03-17 15:23:13 +0100

    move car stats above comments

--- src/routes/posts/[id]/+page.svelte
+++ src/routes/posts/[id]/+page.svelte
@@ -30,6 +30,10 @@
       
             <div class="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
                 <PostDescription title={post.title} info={post.body}/>
+
+                <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">       
+                    <List name="Stats/Specs"/>
+                </div>
       
                 <!-- Options -->
                 <div class="mt-4 lg:row-span-3 lg:mt-0">
@@ -44,9 +48,7 @@
                     </div>
                 </div>
         
-                <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">       
-                    <List name="Stats/Specs"/>
-                </div>
+
             </div>
         </div>
     </div>
